512402be22ac810936016a204809b812595ef4d1
Sergio Triana Escobedo <stescobedo.31@gmail.com>
2021-08-21 17:37:56 -0600

    Adding mcv command

---
+++ GolangAssistant/Commands/MvcCmd.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Threading.Tasks;
+using McMaster.Extensions.CommandLineUtils;
+using Microsoft.Extensions.Logging;
+
+namespace GolangAssistant.Commands
+{
+    [Command(Name = "mvc", Description = "Create a project (Model-View-Controller)")]
+    class MvcCmd : GoaCmdBase
+    {
+        [Option(CommandOptionType.SingleValue, ShortName = "e", LongName = "empty", Description = "project empty", ShowInHelpText = true)]
+        public string Empty { get; set; }
+
+        [Option(CommandOptionType.SingleValue, ShortName = "s", LongName = "server", Description = "project with a basic server", ShowInHelpText = true)]
+        public string Server { get; set; }
+
+        [Option(CommandOptionType.SingleValue, ShortName = "g", LongName = "generate", Description = "generate controller", ShowInHelpText = true)]
+        public string Generate { get; set; }
+
+        public MvcCmd(ILogger<MvcCmd> logger, IConsole console)
+        {
+            _logger = logger;
+            _console = console;
+        }
+
+        protected override async Task<int> OnExecute(CommandLineApplication app)
+        {
+            try
+            {
+                if (string.IsNullOrEmpty(Empty))
+                {
+                    Console.WriteLine("no se puso ninguna opcion");
+                }
+                else
+                {
+                    Console.WriteLine("se puso opcion");
+                }
+
+                return 0;
+            }
+            catch (Exception ex)
+            {
+                OnException(ex);
+                return 1;
+            }
+        }
+    }
+}

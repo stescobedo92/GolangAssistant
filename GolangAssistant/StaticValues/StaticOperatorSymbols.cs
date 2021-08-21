7068e1930a2589bb4cc3769ea2dc9aa19da90850
Sergio Triana Escobedo <stescobedo.31@gmail.com>
2021-08-21 10:32:59 -0600

    Adding static names

---
+++ GolangAssistant/StaticValues/StaticOperatorSymbols.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GolangAssistant.StaticValues
+{
+    public class StaticOperatorSymbols
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        public static string OpenBracket = "{";
+        /// <summary>
+        /// 
+        /// </summary>
+        public static string CloseBracket = "}";
+        /// <summary>
+        /// 
+        /// </summary>
+        public static string OneTab = "\t";
+        /// <summary>
+        /// 
+        /// </summary>
+        public static string Quote = ('"').ToString();
+        /// <summary>
+        /// 
+        /// </summary>
+        public static string BackSlash = "\\";
+    }
+}
---
+++ GolangAssistant/StaticValues/StaticTextNames.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GolangAssistant.StaticValues
+{
+    public class StaticTextNames
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        public static string Package = "package";
+        /// <summary>
+        /// 
+        /// </summary>
+        public static string Source = "src";
+        /// <summary>
+        /// 
+        /// </summary>
+        public static string Extention = ".go";
+        /// <summary>
+        /// 
+        /// </summary>
+        public static string ImportFmt = $"import {StaticOperatorSymbols.Quote}fmt{StaticOperatorSymbols.Quote}";
+        /// <summary>
+        /// 
+        /// </summary>
+        public static string WelcomeMessage = $"{StaticOperatorSymbols.Quote}Hello from Golang!.{StaticOperatorSymbols.Quote}";
+    }
+}

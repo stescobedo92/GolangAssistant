e209ae4eec3ac350847aa0b8aa0e5dcda46770e8
Sergio Triana Escobedo <stescobedo.31@gmail.com>
2021-08-21 10:33:38 -0600

    Added Tools for management the templates and files

---
+++ GolangAssistant/Tools/FileManager.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using GolangAssistant.StaticValues;
+
+namespace GolangAssistant.Tools
+{
+    public class FileManager
+    {
+        public static async Task CreateDefaultProject()
+        {
+            string defaultPath = Directory.GetCurrentDirectory();
+            string defaultDirectory = defaultPath.Substring(defaultPath.LastIndexOf("\\") + 1);
+
+            if (!Directory.Exists(defaultDirectory))
+            {
+                Directory.CreateDirectory(defaultDirectory);
+
+                string pathRoot = Directory.GetCurrentDirectory() + StaticOperatorSymbols.BackSlash + defaultDirectory;
+                string path = pathRoot + StaticOperatorSymbols.BackSlash + StaticTextNames.Source;
+
+                Directory.CreateDirectory(path);
+                string fullPath = path + StaticOperatorSymbols.BackSlash + defaultDirectory + StaticTextNames.Extention;
+
+                string basicTemplate = TemplateManager.CreateBasicTemplate(defaultDirectory).TrimStart();
+
+                if (!File.Exists(fullPath))
+                {
+                    using FileStream fileStream = File.Open(fullPath, FileMode.Create);
+                    using (StreamWriter sw = new StreamWriter(fileStream))
+                    {
+                        sw.WriteLine(basicTemplate);
+                    }
+                }
+            }
+        }
+
+        public static async Task CreateBasicProject(string fileName)
+        {
+            string globalPath = Directory.GetCurrentDirectory() + StaticOperatorSymbols.BackSlash + fileName;            
+            if (!Directory.Exists(globalPath))
+            {
+                Directory.CreateDirectory(globalPath);
+
+                string pathToSrc = globalPath + StaticOperatorSymbols.BackSlash + StaticTextNames.Source;
+                Directory.CreateDirectory(pathToSrc);
+                string fullPath = pathToSrc + StaticOperatorSymbols.BackSlash + fileName + StaticTextNames.Extention;
+
+                string basicTemplate = TemplateManager.CreateBasicTemplate(fileName).TrimStart();
+
+                if (!File.Exists(fullPath))
+                {
+                    using FileStream fileStream = File.Open(fullPath, FileMode.Create);
+                    using (StreamWriter sw = new StreamWriter(fileStream))
+                    {
+                        sw.WriteLine(basicTemplate);
+                    }
+                }
+            }
+        }        
+    }
+}
---
+++ GolangAssistant/Tools/TemplateManager.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using GolangAssistant.StaticValues;
+
+namespace GolangAssistant.Tools
+{
+    public class TemplateManager
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        public static string CreateBasicTemplate(string headerPackageName)
+        {
+            string basicTemplate = $@"
+{StaticTextNames.Package} {headerPackageName}
+
+{StaticTextNames.ImportFmt}
+
+func main() {StaticOperatorSymbols.OpenBracket}
+{StaticOperatorSymbols.OneTab}fmt.Println({StaticTextNames.WelcomeMessage})
+{StaticOperatorSymbols.CloseBracket}
+            ";
+
+            return basicTemplate;
+        }
+    }
+}

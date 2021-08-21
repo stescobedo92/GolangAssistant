3274a2075ba54403006d8a8bea9ed09d273044e9
Sergio Triana Escobedo <stescobedo.31@gmail.com>
2021-08-21 10:33:55 -0600

    Added settings and configurations

--- GolangAssistant/Program.cs
+++ GolangAssistant/Program.cs
@@ -1,12 +1,54 @@
 ﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using GolangAssistant.Commands;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Serilog;
+using Serilog.Extensions.Logging;
 
 namespace GolangAssistant
 {
     class Program
     {
-        static void Main(string[] args)
+        private static async Task<int> Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var Configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile(AppDomain.CurrentDomain.BaseDirectory + "\\goasettings.json", optional: true, reloadOnChange: true)
+                .AddEnvironmentVariables()
+                .Build();
+
+            Log.Logger = new LoggerConfiguration()
+               .ReadFrom.Configuration(Configuration)
+               .Enrich.FromLogContext()
+               .CreateLogger();
+
+            var builder = new HostBuilder().ConfigureServices((hostContext, services) =>
+            {
+                services.AddLogging(config =>
+                {
+                    config.ClearProviders();
+                    config.AddProvider(new SerilogLoggerProvider(Log.Logger));
+                    var minimumLevel = Configuration.GetSection("Serilog:MinimumLevel")?.Value;
+                    if (!string.IsNullOrEmpty(minimumLevel))
+                    {
+                        config.SetMinimumLevel(Enum.Parse<LogLevel>(minimumLevel));
+                    }
+                });
+            });
+
+            try
+            {
+                return await builder.RunCommandLineApplicationAsync<GoaCmd>(args);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return 1;
+            }
         }
     }
 }
